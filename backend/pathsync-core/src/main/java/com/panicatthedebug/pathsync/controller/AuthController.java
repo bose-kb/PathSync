c16fc8cc93537dfe07b7f289105d20cf197b7f58
bose-kb <85823329+bose-kb@users.noreply.github.com>
2025-05-15 23:16:55 +0530

    Add controller for user registration & login.

---
+++ backend/pathsync-core/src/main/java/com/panicatthedebug/pathsync/controller/AuthController.java
@@ -0,0 +1,42 @@
+package com.panicatthedebug.pathsync.controller;
+
+import com.panicatthedebug.pathsync.exception.UnauthorizedException;
+import com.panicatthedebug.pathsync.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    @Autowired
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Object> register(@RequestBody Map<String, String> userData) {
+        return ResponseEntity.ok(userService.registerUser(
+                userData.get("firstName"),
+                userData.get("lastName"),
+                userData.get("email"),
+                userData.get("password")
+        ));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody Map<String, String> loginData) throws UnauthorizedException {
+        return ResponseEntity.ok(userService.loginUser(
+                loginData.get("email"),
+                loginData.get("password")
+        ));
+    }
+}
