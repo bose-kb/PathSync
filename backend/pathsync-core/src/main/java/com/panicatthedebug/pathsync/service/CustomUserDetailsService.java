dd26597cad62f2a0d9d9698fc99759c32bb9e4f1
bose-kb <85823329+bose-kb@users.noreply.github.com>
2025-05-15 23:20:59 +0530

    Add implementation class for UserDetailsService interface required by Spring.

---
+++ backend/pathsync-core/src/main/java/com/panicatthedebug/pathsync/service/CustomUserDetailsService.java
@@ -0,0 +1,52 @@
+package com.panicatthedebug.pathsync.service;
+
+import com.panicatthedebug.pathsync.model.User;
+import com.panicatthedebug.pathsync.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepo userRepo;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        final User user;
+        try {
+            user = userRepo.findByEmail(userEmail);
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("User doesn't exist.");
+        }
+
+        return new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return Collections.singleton(new SimpleGrantedAuthority(user.getRole()));
+            }
+
+            @Override
+            public String getPassword() {
+                return user.getPassword();
+            }
+
+            @Override
+            public String getUsername() {
+                return userEmail;
+            }
+        };
+    }
+}
