6d9d7ca6df4c2602ae1fb97944ae58cbe0d63b1e
debarkac <debarka.2k2@gmail.com>
2025-05-16 04:54:02 +0530

    Frontend setup

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,11 @@
+export interface SurveyData {
+  target: string;
+  language: string;
+  answers: Record<string, string>;
+}
+
+export interface Question {
+  id: string;
+  text: string;
+  options: string[];
+}
