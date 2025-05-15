9fd283b0b2dfc620a550b15155db5606acc4ec75
bose-kb <85823329+bose-kb@users.noreply.github.com>
2025-05-15 23:18:05 +0530

    Add new exception class.

---
+++ backend/pathsync-core/src/main/java/com/panicatthedebug/pathsync/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.panicatthedebug.pathsync.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
