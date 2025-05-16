e5e999843e8207a50506555468120a7b5c1aaacf
Skew008 <shreyanshtelrandhe234@gmail.com>
2025-05-17 00:10:23 +0530

    Added Authentication

--- backend/pathsync-core/src/main/java/com/panicatthedebug/pathsync/controller/ResumeController.java
+++ backend/pathsync-core/src/main/java/com/panicatthedebug/pathsync/controller/ResumeController.java
@@ -4,6 +4,7 @@ import com.panicatthedebug.pathsync.model.ResumeDetails;
 import com.panicatthedebug.pathsync.service.ResumeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +19,9 @@ public class ResumeController {
 
 
     @PostMapping("upload")
-    public ResponseEntity<ResumeDetails> uploadResume(MultipartFile file) {
+    public ResponseEntity<ResumeDetails> uploadResume(Authentication authentication, MultipartFile file) {
         try {
-            ResumeDetails resumeDetails = resumeService.parseAndSaveResume(file);
+            ResumeDetails resumeDetails = resumeService.parseAndSaveResume(file, authentication.getName());
             return ResponseEntity.ok(resumeDetails);
         } catch (Exception e) {
             return ResponseEntity.status(500).body(null);
