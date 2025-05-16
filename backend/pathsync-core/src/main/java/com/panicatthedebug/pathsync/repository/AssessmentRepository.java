9db5b3f17bfd6a6ea045abed9d2e5dc966093765
codersidhant <sidhant.guha@gmail.com>
2025-05-17 01:48:43 +0530

    Modify AssessmentService to include a function to get list of assessments

--- backend/pathsync-core/src/main/java/com/panicatthedebug/pathsync/repository/AssessmentRepository.java
+++ backend/pathsync-core/src/main/java/com/panicatthedebug/pathsync/repository/AssessmentRepository.java
@@ -4,9 +4,11 @@ import com.panicatthedebug.pathsync.model.Assessment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AssessmentRepository extends MongoRepository<Assessment, String> {
     Optional<Assessment> findByUserEmailAndCompletedFalse(String userEmail);
+    List<Assessment> findByUserEmail(String userEmail);
 }
