3afdcf475b7039fc8c1f17ab47fbb9766f9ce4f8
bose-kb <85823329+bose-kb@users.noreply.github.com>
2025-05-16 23:45:45 +0530

    Update global exception handler to include new exceptions.

--- backend/pathsync-core/src/main/java/com/panicatthedebug/pathsync/exception/GlobalExceptionHandler.java
+++ backend/pathsync-core/src/main/java/com/panicatthedebug/pathsync/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.panicatthedebug.pathsync.exception;
 
+import com.mongodb.lang.NonNull;
+import com.mongodb.lang.NonNullApi;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -12,12 +15,26 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
-     static final String MESSAGE = "message";
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    static final String MESSAGE = "message";
+
+//    @Override
+//    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+//            @NonNull MethodArgumentNotValidException ex,
+//            @NonNull HttpHeaders headers,
+//            @NonNull HttpStatusCode status,
+//            @NonNull WebRequest request) {
+//        return handleExceptionInternal(ex, Map.of(MESSAGE, ex.getMessage()),
+//                headers,
+//                HttpStatus.BAD_REQUEST,
+//                request
+//        );
+//    }
 
     @ExceptionHandler({
             UserAlreadyExistsException.class,
-            ValidationException.class
+            ValidationException.class,
+            SurveyNotCompleteException.class
     })
     public ResponseEntity<Object> handleBadRequest(Exception e, WebRequest webRequest) {
         return handleExceptionInternal(e, Map.of(MESSAGE, e.getMessage()),
@@ -29,6 +46,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 
     @ExceptionHandler({
             UserNotFoundException.class,
+            LearnPathNotFoundException.class,
+            QuestionNotFoundException.class
     })
     public ResponseEntity<Object> handleNotFound(Exception e, WebRequest webRequest) {
         return handleExceptionInternal(e, Map.of(MESSAGE, e.getMessage()),
@@ -53,6 +72,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
             Exception.class,
     })
     public ResponseEntity<Object> handleServerError(Exception e, WebRequest webRequest) {
+        e.printStackTrace();
         return handleExceptionInternal(e, Map.of(MESSAGE, "An unknown error occurred"),
                 new HttpHeaders(),
                 HttpStatus.INTERNAL_SERVER_ERROR,
